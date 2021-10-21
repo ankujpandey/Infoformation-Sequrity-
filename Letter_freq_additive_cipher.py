8cd59180285e644c82fb29f4ed5ce3dde551cdc3
ankujpandey <82190152+ankujpandey@users.noreply.github.com>
2021-10-21 22:48:46 +0530

    Update Letter_freq_additive_cipher.py

--- Letter_freq_additive_cipher.py
+++ Letter_freq_additive_cipher.py
@@ -1,4 +1,8 @@
-
+'''
+Write a program that can perform a letter frequency attack on an additive cipher without human intervention. 
+Your software should produce possible plain text in rough order of likelihood.
+It would be good if your user interface allows user to specify " Give me top 10 possible plain texts"
+'''
 def maxFriquency(str):
     '''
     Purpose:- This function is desined to find the maximum frequency of the letters and then to throw the letter which occured most.
